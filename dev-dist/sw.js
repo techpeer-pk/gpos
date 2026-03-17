365e4ab4f18945511794dcc0a39488003e015ff8
Silverado313 <goodeedproduction@gmail.com>
2026-03-17 13:49:16 +0500

    feat: implement products management page with full CRUD, search, sort, and pagination capabilities, and add service worker.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.bnj7ube0lrk"
+    "revision": "0.ev2ldsh3ogo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
