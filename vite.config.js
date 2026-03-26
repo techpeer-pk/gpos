677cbc0c72c750cc4d75d37f6dc8a4b6cb5c0b3e
Silverado313 <goodeedproduction@gmail.com>
2026-03-26 13:39:39 +0500

    Update default favicon by Vite

--- vite.config.js
+++ vite.config.js
@@ -25,25 +25,25 @@ export default defineConfig({
             sizes: 'any',
             type: 'image/svg+xml',
             purpose: 'any'
-          },
-          {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
           }
+          // {
+          //   src: '/pwa-192x192.png',
+          //   sizes: '192x192',
+          //   type: 'image/png',
+          //   purpose: 'any'
+          // },
+          // {
+          //   src: '/pwa-512x512.png',
+          //   sizes: '512x512',
+          //   type: 'image/png',
+          //   purpose: 'any'
+          // },
+          // {
+          //   src: '/pwa-512x512.png',
+          //   sizes: '512x512',
+          //   type: 'image/png',
+          //   purpose: 'maskable'
+          // }
         ]
       },
       workbox: {
